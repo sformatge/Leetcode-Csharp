1c37b8afe12d1b24925e2b0634abd492effe84cc
sformatge <sergiformatge@gmail.com>
2024-09-10 17:31:33 +0200

    Add project files.

---
+++ LeetCode3/Arrays.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode3
+{
+    public class Arrays
+    {
+        public void DuplicateZeros(int[] arr)
+        {
+            int[] support = new int[arr.Length];
+            int iteradorSupport = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] == 0 && arr[i] <= arr.Length && iteradorSupport <= support.Length-1)
+                {
+                    support[iteradorSupport] = 0;
+                    iteradorSupport++;
+                    if (iteradorSupport <= support.Length - 1)
+                    {
+                        support[iteradorSupport] = 0;
+                        iteradorSupport++;
+                    }
+                } else if (iteradorSupport < support.Length)
+                {
+                    support[iteradorSupport] = arr[i];
+                    iteradorSupport++;
+                }
+            }
+
+            support.CopyTo(arr, 0);
+        }
+        public void Merge(int[] nums1, int m, int[] nums2, int n)
+        {
+            Array.Copy(nums2, 0, nums1, m, n);
+            Array.Sort(nums1);
+            Console.WriteLine();
+            foreach (int item in nums1)
+            {
+                Console.Write(item);
+            }
+        }
+        public int RemoveElement(int[] nums, int val)
+        {
+            int k = 0;
+            int[] numsTemp = new int[nums.Length];
+            int indexTemp = 0;
+
+            for (int i = 0; i< nums.Length; i++)
+            {
+                if (nums[i] != val)
+                {
+                    k++;
+                    numsTemp[indexTemp] = nums[i];
+                    indexTemp++;
+                }
+            }
+            for (int b = 0; b < nums.Length; b++)
+            {
+                nums[b] = numsTemp[b];
+            }
+            foreach (int item in nums)
+            {
+                Console.Write(item + ", ");
+            }
+            
+            return k;
+        }
+        public int RemoveDuplicates(int[] nums)
+        {
+            int k = 0;
+            int[] numsTemp = new int[nums.Length];
+            int indexTemp = 0;
+            int previousNum = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] != previousNum || i == 0)
+                {
+                    k++;
+                    numsTemp[indexTemp] = nums[i];
+                    indexTemp++;
+                    previousNum = nums[i];
+                }
+            }
+            for (int b = 0; b < nums.Length; b++)
+            {
+                nums[b] = numsTemp[b];
+            }
+            return k;
+        }
+        public bool CheckIfExist(int[] arr)
+        {
+            for (int i =0; i < arr.Length; i++)
+            {
+                for (int j = 0; j < arr.Length; j++)
+                {
+                    if (i != j && 0<= i && j < arr.Length && arr[i] == 2 * arr[j])
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+        public bool ValidMountainArray(int[] arr)
+        {
+            bool res = false;
+            bool pujant = true;
+
+            if (arr.Length < 3) {return false;}
+            if (arr[0] >= arr[1]){return false;}
+            if (arr[arr.Length - 1] >= arr[arr.Length -2 ]){return false;}
+
+            for (int i = 1; i< arr.Length; i++)
+            {
+                if (arr[i] == arr[i - 1]) { return false; }
+                else if (arr[i] > arr[i - 1] && pujant)
+                {
+                    res = true;
+                }
+                else if (arr[i] < arr[i - 1])
+                {
+                    pujant = false;
+                    res = true;
+                }
+                else
+                {
+                    return false;
+                }
+
+            }
+            return res;
+        }
+        public int[] ReplaceElements(int[] arr)
+        {
+            int[] temp = new int[arr.Length];
+
+            for (int i = 0; i<arr.Length; i++)
+            {
+                int tempValorMax = 0;
+                for (int j = i + 1; j < arr.Length; j++)
+                {
+                    if (arr[j] > tempValorMax)
+                    {
+                        tempValorMax = arr[j]; 
+                    }
+                }
+                if (i == arr.Length - 1)
+                {
+                    temp[i] = -1;
+                } else
+                {
+                    temp[i] = tempValorMax;
+                }
+            }
+            for (int b = 0; b < arr.Length; b++)
+            {
+                arr[b] = temp[b];
+            }
+            return arr;
+
+        }
+        public void MoveZeroes(int[] nums)
+        {
+            int[] temp = new int [nums.Length];
+            int counterTemp = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] != 0)
+                {
+                    temp[counterTemp] = nums[i];
+                    counterTemp++;  
+                }
+            }
+
+            for (int b = 0; b < nums.Length; b++)
+            {
+                nums[b] = temp[b];
+            }
+        }
+        public int[] SortArrayByParity(int[] nums)
+        {
+            int secondCounter = 0;
+            int fromLastCount = nums.Length - 1;
+            int[] temp = new int[nums.Length];
+            for (int i = 0; i < nums.Length; i++) 
+            { 
+                if (nums[i] % 2 == 0)
+                {
+                    temp[secondCounter] = nums[i];
+                    secondCounter++;
+                } else
+                {
+                    temp[fromLastCount] = nums[i];
+                    fromLastCount--;
+                }
+            }
+            for (int b = 0; b < nums.Length; b++)
+            {
+                nums[b] = temp[b];
+            }
+            return nums;
+        }
+    }
+
+}
---
+++ LeetCode3/Solucio.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace LeetCode3
+{
+    public class Solucio
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Main");
+            Arrays testArrays = new Arrays();
+
+            /* DuplicateZeros */
+            //int[] toTest = new int[8] { 1, 0, 2, 3, 0, 4, 5, 0 };
+            //int[] toTest2 = new int[7] { 0, 0, 0, 0, 0, 0, 0 };
+            //testArrays.DuplicateZeros(toTest);
+            //Console.WriteLine("");
+            //testArrays.DuplicateZeros(toTest2);
+            //Console.WriteLine("");
+
+            /* Merge Sorted Array*/
+            //int[] nums1 = new int[6] { 1, 2, 3, 0, 0, 0 };
+            //int[] nums2 = new int[3] { 2, 5, 6 };
+            //int m = 3, n = 3;
+            //testArrays.Merge(nums1, m, nums2, n);
+
+            /* Remove Element*/
+            //int[] nums = new int[4] {3,2,2,3};
+            //int val = 3;
+            //testArrays.RemoveElement(nums, val);
+
+            /* Remove Duplicate */
+            //int[] nums = new int[3] { 1, 1, 2 };
+            //testArrays.RemoveDuplicates(nums);
+
+            /* Check If N and Its Double Exist */
+            //int[] arr = new int[4] { 10, 2, 5, 3 };
+            //Console.WriteLine(testArrays.CheckIfExist(arr));
+            //int[] arr2 = new int[4] { 3, 1, 7, 11 };
+            //Console.WriteLine(testArrays.CheckIfExist(arr2));
+
+            /* Valid Mountain Array */
+            //int[] arr = new int[4] { 0, 3, 2, 1 };
+            //Console.WriteLine(testArrays.ValidMountainArray(arr));
+            //int[] arr2 = new int[10] { 0, 1, 2, 3, 2, 1, 0, 1, 2, 0 };
+            //Console.WriteLine(testArrays.ValidMountainArray(arr2));
+            //int[] arr3 = new int[10] { 9, 8, 7, 6, 5, 4, 3, 2, 1, 0 };
+            //Console.WriteLine(testArrays.ValidMountainArray(arr3));
+
+            /* Replace Elements with Greatest Element on Right Side */
+            //int[] arr = new int[6] { 17, 18, 5, 4, 6, 1 };
+            //Console.WriteLine(testArrays.ReplaceElements(arr));
+            //int[] arr1 = new int[1] { 400 };
+            //Console.WriteLine(testArrays.ReplaceElements(arr1));
+
+            /* Move zeroes to end */
+            //int[] arr = new int[5] { 0, 1, 0, 3, 12 };
+            //testArrays.MoveZeroes(arr);
+
+            /* Move zeroes to end */
+            int[] arr = new int[4] { 3,1,2,4 };
+            Console.WriteLine(testArrays.SortArrayByParity(arr));
+
+
+            Console.WriteLine("");
+        }
+    }
+}
